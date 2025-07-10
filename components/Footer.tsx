79e87cbe45e1d618e33df77798b2bc7165a42a22
Asif Ali <aali9548702@gmail.com>
2025-07-10 23:48:39 +0530

    add Footer component with design credits

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+
+export const Footer = () => {
+    return (
+        <section className="max-w-7xl mx-auto border-t px-4">
+            <div className="flex justify-between py-8">
+                <p className="text-primary tracking-tight">
+                Designed and Developed by{" "}
+                    <span className="font-bold">Asif</span>
+                </p>
+            </div>
+        </section>
+    );
+}
